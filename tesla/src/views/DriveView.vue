9a1a8d92023aef77101041d15490736955d5225e
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2023-01-13 00:57:20 +0100

    Nuova pagina testDrive

---
+++ tesla/src/views/DriveView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <head>
+            <title>Prenota un test drive | Tesla</title>
+        </head>
+
+        TEST DRIVE
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DriveView',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
