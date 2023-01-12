7cb29e8e0ea402472b97897763dd3582c93d860f
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2023-01-12 17:47:57 +0100

    Inizializzazione Vue

---
+++ tesla/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    prova
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+  margin: 0px;
+  padding: 0px;
+}
+
+body {
+  font-family: sans-serif;
+}
+</style>
